676099bfe95f321bf01471e0c339c20cbabfe4b3
Ratson <github@ratson.name>
2016-06-22 01:12:19 +0800

    Remove unused import and reformat code

--- android/src/main/java/com/babisoft/ReactNativeLocalization/ReactNativeLocalizationPackage.java
+++ android/src/main/java/com/babisoft/ReactNativeLocalization/ReactNativeLocalizationPackage.java
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
