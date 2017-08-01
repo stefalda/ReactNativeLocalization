988697de304deec689c295c7f9cb8daf8f87d21d
Felipe Cavalcante Constantino <felipe.constantino@linux.autbank>
2017-08-01 15:43:10 -0300

    refactor(typings): removed namespace + set default export as const

--- lib/LocalizedStrings.d.ts
+++ lib/LocalizedStrings.d.ts
@@ -1,63 +1,60 @@
 declare module 'react-native-localization' {
+    //
+    // var strings = LocalizedStrings({ "en": { "Hello": "Hello" }})
+    //
+    // strings.getLanguage()
+    //
+    export interface LocalizationStringsApi {
+        getLanguage(): string;
 
-    namespace ReactNativeLocalization {
-        //
-        // var strings = LocalizedStrings({ "en": { "Hello": "Hello" }})
-        //
-        // strings.getLanguage()
-        //
-        export interface LocalizationStringsApi {
-            getLanguage(): string;
+        setLanguage(language: string): void;
 
-            setLanguage(language: string): void;
+        getInterfaceLanguage(): string;
 
-            getInterfaceLanguage(): string;
+        getAvailableLanguages(): string[];
 
-            getAvailableLanguages(): string[];
+        formatString(str: string, ...values: string[]): string;
 
-            formatString(str: string, ...values: string[]): string;
-
-            getString(key: string, language: string): string;
-        }
-
-        // Define input strings:
-        //
-        //  for example:
-        //
-        //  import LocalizedStrings, {LocalizationStringsApi, GlobalStrings} from 'react-native-localization'
-        //
-        //  interface MyStrings {
-        //      hello: string;
-        //      world: string;
-        //  }
-        //
-        //  The strings in English and french
-        //
-        //  const myGlobalStrings: GlobalStrings<MyStrings> = {
-        //      "en": {
-        //          hello: "Hello",
-        //          world: "World"
-        //      },
-        //      "fr": {
-        //          hello: "Bonjour",
-        //          world: "le monde"
-        //      }
-        //  }
-        interface GlobalStrings<T> {
-            [language: string]: T;
-        }
-
-        // To get a localized version
-        //
-        // export default new LocalizedStrings(myGlobalStrings) as any as LocalizationStringsApi & MyStrings
-        //
-        // The reason for the above kludge is the fact that the exported strings is a type that is both
-        // my strings in adition to the functions exposed by the localizedStrings class
-        //
-        export default interface LocalizedStrings {
-            new<T>(globalStrings: GlobalStrings<T>): LocalizationStringsApi & GlobalStrings<T>;
-        }
+        getString(key: string, language: string): string;
     }
 
-    export = ReactNativeLocalization;
+    // Define input strings:
+    //
+    //  for example:
+    //
+    //  import LocalizedStrings, {LocalizationStringsApi, GlobalStrings} from 'react-native-localization'
+    //
+    //  interface MyStrings {
+    //      hello: string;
+    //      world: string;
+    //  }
+    //
+    //  The strings in English and french
+    //
+    //  const myGlobalStrings: GlobalStrings<MyStrings> = {
+    //      "en": {
+    //          hello: "Hello",
+    //          world: "World"
+    //      },
+    //      "fr": {
+    //          hello: "Bonjour",
+    //          world: "le monde"
+    //      }
+    //  }
+    interface GlobalStrings<T> {
+        [language: string]: T;
+    }
+
+    // To get a localized version
+    //
+    // export default new LocalizedStrings(myGlobalStrings) as any as LocalizationStringsApi & MyStrings
+    //
+    // The reason for the above kludge is the fact that the exported strings is a type that is both
+    // my strings in adition to the functions exposed by the localizedStrings class
+    //
+    interface ILocalizedStrings {
+        new<T>(globalStrings: GlobalStrings<T>): LocalizationStringsApi & T;
+    }
+    const LocalizedStrings: ILocalizedStrings;
+    export default LocalizedStrings;
 }
