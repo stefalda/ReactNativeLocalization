5d22ef0595601c256a36f10b83c57dabb8ae3803
alphasp <gmerudotcom@gmail.com>
2017-08-04 21:29:16 +0800

    Remove createJSModules override - RN 0.47 compatibility

--- android/src/main/java/com/babisoft/ReactNativeLocalization/ReactNativeLocalizationPackage.java
+++ android/src/main/java/com/babisoft/ReactNativeLocalization/ReactNativeLocalizationPackage.java
@@ -24,7 +24,7 @@ public class ReactNativeLocalizationPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
