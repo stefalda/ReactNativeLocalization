45cebfacdc3a1b4cfc509be90e26cb656f015252
Stefano Falda <stefano.falda@gmail.com>
2018-10-27 00:15:29 +0200

    Fixes compatibilty with latest version of localized-strings

--- src/LocalizedStrings.js
+++ src/LocalizedStrings.js
@@ -11,34 +11,35 @@
  * Check the instructions at:
  * https://github.com/stefalda/ReactNativeLocalization
  */
-'use strict';
-import { NativeModules } from 'react-native';
-import LocalizedStringsCore from 'react-localization';
-/** 
+"use strict";
+import { NativeModules } from "react-native";
+import LocalizedStringsCore from "react-localization";
+/**
  * Import the native module
-*/
+ */
 const localization = NativeModules.ReactLocalization;
 /**
  * Check if the native module has been correctly initialized
  */
 if (!localization) {
-    console.error("Something went wrong initializing the native ReactNativeLocalization module.\nPlease check your configuration.\nDid you run 'react-native link'?");
+  console.error(
+    "Something went wrong initializing the native ReactNativeLocalization module.\nPlease check your configuration.\nDid you run 'react-native link'?"
+  );
 }
 
-
-/** 
+/**
  * Custom Interface Language method returned by native code
-*/
-function getInterfaceLanguage(){
-    return localization.language.replace(/_/g, '-');
-} 
+ */
+function getInterfaceLanguage() {
+  return localization.language.replace(/_/g, "-");
+}
 
-/** 
-* Extend the react-localization class overriding the getInterfaceLanguage method
-* to use the native module
-*/
-export default class LocalizedStrings extends LocalizedStringsCore{
-  constructor(props){
-    super(props, getInterfaceLanguage);
+/**
+ * Extend the react-localization class overriding the getInterfaceLanguage method
+ * to use the native module
+ */
+export default class LocalizedStrings extends LocalizedStringsCore {
+  constructor(props) {
+    super(props, { customLanguageInterface: getInterfaceLanguage });
   }
 }
